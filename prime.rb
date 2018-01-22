037825774bbb73224675bf138205b45aada2c2c5
John Daise <jdaise@gmail.com>
2018-01-22 03:01:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,7 @@
 # Add  code here!
 def prime?(i)
   array = (1..i**0.5).to_a
-  array.each do |f|
-    if i%f != 0
-    else
-      false
-    end
+  array.none? do |element|
+    i%element = 0  
   end
 end
