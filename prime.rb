e445bdf14f2c6a47c972503b65d4e7c2b4f75103
John Daise <jdaise@gmail.com>
2018-01-22 03:12:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 # Add  code here!
 def prime?(n)
-  n>0
-  array = (2..n**0.5).to_a
-  array.none? do |b|
-    n%b == 0
-  end
+  if n>0
+    array = (2..n**0.5).to_a
+    array.none? do |b|
+      n%b == 0
+    end
+  elsif n<=0
+    false
+
 end
